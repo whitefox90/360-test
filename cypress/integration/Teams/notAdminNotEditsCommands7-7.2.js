e725798501f729d8262f5c562a7a2a040d8424e1
Marina <belayalisica@gmail.com>
2021-03-17 14:19:25 +0300

    #7Возможность администратора редактировать команды

--- cypress/integration/Teams/notAdminNotEditsCommands7-7.2.js
+++ cypress/integration/Teams/notAdminNotEditsCommands7-7.2.js
@@ -1,15 +1,13 @@
-const {nameWithSmallLetter}=require('./const/nameTeams');
-const {rightUsername}=require('../authorization/const/username');
-const {rightPassword}=require('../authorization/const/password');
+const {nameWithSmallLetter} = require('./const/nameTeams');
+const {rightUsername} = require('../authorization/const/username');
+const {rightPassword} = require('../authorization/const/password');
+const buttonNotFound = '#root > div > nav > div.navbar-nav.mr-auto > li:nth-child(3) > a';
 
 
-
-
-describe('Не возможность не администратора редактировать команды',()=> {
+describe('Не возможность не администратора редактировать команды', () => {
     it('By', () => {
         cy.visit("/");
-        cy.get('#root > div > nav > div.navbar-nav.ml-auto > li:nth-child(1) > a').click()
-        cy.get('#root > div > nav > div.navbar-nav.mr-auto > li:nth-child(3) > a').click()
+        cy.get('[href="/teams"]').should('have.length', 0)
     })
 })
 
