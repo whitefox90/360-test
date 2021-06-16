55230452c422c508a3fb4330485def4d26cf9802
Marina <belayalisica@gmail.com>
2021-06-16 21:36:31 +0300

    remove extra comments

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,12 @@
 const mysql = require('mysql2')
 function queryTestDb(query, config) {
-    // creates a new mysql connection using credentials from cypress.json env's
     const connection = mysql.createConnection(config.env.db)
-    // start connection to db
     connection.connect()
-    // exec query + disconnect to db as a Promise
     return new Promise((resolve, reject) => {
         connection.query(query, (error, results) => {
             if (error) reject(error)
             else {
                 connection.end()
-                // console.log(results)
                 return resolve(results)
             }
         })
